fe82e9399782cd52e14dd9a146bfce820793af0b
Juan Pablo Solana Ortiz <juanpablosolana@gmail.com>
2021-05-21 12:18:53 -0500

    Fixed title colors

--- src/components/curses.js
+++ src/components/curses.js
@@ -29,14 +29,11 @@ function Courses(){
           className="min-h-screen container mx-auto p-12 max-w-6xl "
           id="Cursos"
         >
-          <div className="text-center">
+          <div className="text-center text-gray-100 ">
             <h2 className="text-4xl font-bold">
-              Parece magia, funciona con código.
+              Me preparo con los mejores
             </h2>
-            <p className="p-4">
-              A lo largo de mi carrera como programador, he tenido el privilegio
-              de trabajar en proyectos retadores e increíbles.
-            </p>
+            <p className="p-4 ">Sin dudarlo ¡Te los recomiendo!</p>
           </div>
 
           <div className="grid grid-cols-3 transparente5">
@@ -47,23 +44,23 @@ function Courses(){
                   className="shadown p-8 bg-gray-100 mx-4 rounded my-4 text-center"
                 >
                   <h4 className="font-bold p-2 ">{courses.title}</h4>
-                  <span className="inline-block bg-yellow-500 p-2 rounded mb-0.5 bg-red-500 radius">
+                    <span className="inline-block bg-yellow-500 p-2 rounded mb-0.5 bg-red-500 radius">
+                      <a
+                        href={courses.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Ir al curso de {courses.author}
+                      </a>
+                    </span>
                     <a
-                      href={courses.url}
+                      href={courses.twitterLink}
                       target="_blank"
                       rel="noopener noreferrer"
+                      className="text-blue-500"
                     >
-                      Ir al curso de {courses.author}
+                      Seguir {courses.twitter}
                     </a>
-                  </span>
-                  <a
-                    href={courses.twitterLink}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-blue-500"
-                  >
-                    Seguir {courses.twitter}
-                  </a>
                 </div>
               )
             })}

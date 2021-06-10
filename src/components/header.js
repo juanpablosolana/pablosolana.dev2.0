451b6838c5fbf83c000c633da7bba1be4f471eb3
Juan Pablo Solana Ortiz <juanpablosolana@gmail.com>
2021-06-10 17:07:27 -0500

    new header

--- src/components/header.js
+++ src/components/header.js
@@ -1,25 +1,9 @@
-import React, { useState, useEffect, useRef } from "react"
+import React from "react"
 import Menu from "./menu"
 import profile from "../img/profile.png"
-import useTypewriter from "react-typewriter-hook"
 import { motion } from "framer-motion"
 
-const work_title = ["Developer que le encanta implementar diseños que inspiran y atraen a las personas",
-                    "especialista en WPO."]
-let index = 0
 const Header = () => {
-   const [workTitle, setWorkTitle] = useState("desarrolladora web.")
-   const intervalRef = useRef({})
-   const name = useTypewriter(workTitle)
-   useEffect(() => {
-     intervalRef.current = setInterval(() => {
-       index = index > 2 ? 0 : ++index
-       setWorkTitle(work_title[index])
-     }, 5000)
-     return function clear() {
-       clearInterval(intervalRef.current)
-     }
-   }, [workTitle])
   return (
     <header className="min-h-screen bg-black " id="hola">
       <Menu />
@@ -27,12 +11,15 @@ const Header = () => {
         <div className="grid items-center justify-center grid-rows-1 place-items-center lg:flex">
           <div className="flex-1 text-center">
             <p className="text-xl font-bold text-white lg:text-7xl">
-              ¡Hola a todos!
+              ¡Hola a todos soy!
             </p>
             <h1 className="block py-6 text-xl font-bold text-white lg:text-7xl">
-              Soy Pablo Solana
+              Juan Pablo Solana
             </h1>
-            <p className="py-6 text-xl font-light text-gray-400">{name}</p>
+            <p className="py-6 text-xl font-light text-gray-400">
+              Developer que le encanta implementar diseños que inspiran y atraen
+              a las personas
+            </p>
           </div>
 
           <motion.img

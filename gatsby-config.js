32d513382a0f3737cb08c09ce60860de7210d9b8
Juan Pablo Solana Ortiz <juanpablosolana@gmail.com>
2021-06-11 13:08:39 -0500

    scroll indicator

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,5 +14,31 @@ module.exports = {
         path: `${__dirname}/src/data/`,
       },
     },
+    {
+      resolve: `gatsby-plugin-scroll-indicator`,
+      options: {
+        // set the color you want for your progress bar here
+        color: "#663391",
+        // set the height of the scroll indicator
+        height: "5px",
+        // Set specific paths where you want the scroll indicator using regex
+        paths: ["/", "/blog/**"],
+        // Configure the z-index of the indicator element
+        zIndex: `9999`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "G-MKM16EGNFH",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Defers execution of google analytics script after page load
+        defer: false,
+      },
+    },
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Repos from '../components/repos'
 import Curses from '../components/curses'
 import Certification from '../components/certifications'
 import Form from '../components/form'
+// import scrollTo from "gatsby-plugin-smoothscroll"
 
 export default function Home() {
   return(

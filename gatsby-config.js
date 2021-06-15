72cce9fde488fe01925f35eca8c33ed392368af2
Juan Pablo Solana Ortiz <juanpablosolana@gmail.com>
2021-06-15 08:32:23 -0500

    Gtag ok

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
         // set the color you want for your progress bar here
         color: "#663391",
         // set the height of the scroll indicator
-        height: "5px",
+        height: "6px",
         // Set specific paths where you want the scroll indicator using regex
         paths: ["/", "/blog/**"],
         // Configure the z-index of the indicator element
@@ -28,16 +28,28 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "G-MKM16EGNFH",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Defers execution of google analytics script after page load
-        defer: false,
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "GA-TRACKING_ID", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "G-MKM16EGNFH",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
       },
     },
   ],
